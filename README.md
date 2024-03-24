9c4e853835cb78c224a395fb24a45b253a1b4288
shubham <shubham.indy@gmail.com>
2024-03-24 16:36:17 +0530

    chatroom is completed

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # chat_app_with_python_and_socket_io
+
+This is a chatroom web-application. which is used to do group chat. There is no use of any databases because in This application there is no need to store messages.
+
+I have used HTML, CSS, and Javascript for the fronted and Flask for the backend.
+I also used socketIO for realtime communication.
+
+tools I have used :-
+
+    flask              3.0.2  
+    Flask-SocketIO     5.3.6  
+
--- app.py
+++ app.py
@@ -13,8 +13,8 @@ def index():
     session.clear()
     if request.method == "POST":
         name = request.form.get("name")
-        code = request.form.get("code", False)
-        room_id = request.form.get("room_id")
+        code = request.form.get("join_code", False)
+        room_id = request.form.get("create_room_id")
         join = request.form.get("join",  False)
         create = request.form.get("create", False)
 
@@ -25,9 +25,13 @@ def index():
 
         elif code not in rooms:
             return render_template("index.html", error="Please Enter a valid Room Id to Join", name=name)
-
+        
+        if code == "" and room_id != "" :
+            session["room"] = room_id
+        elif code != "" and room_id == "":
+            session["room"] = code
         session["name"] = name
-        session["room"] = room_id       
+     
 
         print('rooms', rooms)
 
@@ -38,9 +42,10 @@ def index():
 
 @app.route("/chat")
 def chat():
+
     if not session:
         return redirect('/')
-    return render_template("chat.html", name=session["name"], room_id=session["room"])
+    return render_template("chat.html", name=session.get("name"), room_id=session.get("room"))
 
 
 
@@ -49,6 +54,58 @@ def chat():
 def existRoom():
     session.clear()
     return redirect('/')
+
+
+
+
+@socketio.on("connect")
+def connect(auth):
+    room = session.get("room")
+    name = session.get("name")
+
+    if not room or not name:
+        return
+    
+    if room not in rooms:
+        leave_room(room)
+        return
+
+    print(room, name )
+    join_room(room)
+    send({"name":name, "message":"has entered the room "}, to=room)
+    rooms[room]["members"] +=1
+    # print(f"{name} has entered the room")
+
+
+@socketio.on("disconnect")
+def disconnect():
+    room = session.get("room")
+    name = session.get("name")
+    
+    leave_room(room)
+
+    if room in rooms:
+        rooms[room]["members"] -= 1
+        if rooms[room]["members"] <= 0 :
+            del rooms[room]
+
+    send({"name":name, "message":"has left the room "}, to=room)
+    # print(f"{name} has left the room")
+
+
+
+@socketio.on("message")
+def message(data):
+    room = session.get("room")
+    name = session.get("name")
+    
+    if room not in rooms:
+        return
+
+
+    send({"name":name, "message": data['data']}, to=room)
+    rooms[room]["messages"].append({"name":name, "message": data['data']})
+    print(f"Msg Data is {data}")
 
 
 
